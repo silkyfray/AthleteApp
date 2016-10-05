344a1e551c1e156a2003c572695a79529153e6a4
Antonio Nikolov <anikolovn@gmail.com>
2016-10-05 22:26:37 +0100

    Now can fetch the athlete time and return it to the frontend.

--- prototype/src/server.js
+++ prototype/src/server.js
@@ -12,7 +12,8 @@ app.get('/scrape', function(req, res){
     // The first parameter is our URL
     // The callback function takes 3 parameters, an error, response status code and the html
 
-    request.post({url: url, form: {race: "66"}}, function(error, response, html){
+    var atheleteTime = "";
+    request.post({url: url, form: {search:"1", race: "66", number: "1"}}, function(error, response, html){
 
         // First we'll check to make sure no errors occurred when making the request
 
@@ -20,7 +21,9 @@ app.get('/scrape', function(req, res){
             // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 
             var $ = cheerio.load(html);
-            var x = 0;
+            athleteTime = $("#content > table.results_table").children().last().children().last().text();
+            console.log("Athelete time: " + athleteTime);
+            res.send("<h1>athleteTime</h1>" + athleteTime.toString());
         }
     })
 
